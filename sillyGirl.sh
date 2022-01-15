5025462307543b0fef87623bab219f8f4c9dc52d
ZQCCBOY <97740114+ZQCCBOY@users.noreply.github.com>
2022-01-15 14:07:35 +0800

    Update sillyGirl.sh

--- sillyGirl.sh
+++ sillyGirl.sh
@@ -114,10 +114,10 @@ sillyGirl()
     fi
     echo "进入develop文件夹，开始克隆常用库、返利库"
     #克隆一些必要的库
-    git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_jd.git;
-    git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_tb.git;
-    git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_pdd.git;
-    git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_vip.git;
+    git clone https://ghproxy.com/https://github.com/ZQCCBOY/fanli_jd.git;
+    git clone https://ghproxy.com/https://github.com/ZQCCBOY/fanli_tb.git;
+    git clone https://ghproxy.com/https://github.com/ZQCCBOY/fanli_pdd.git;
+    git clone https://ghproxy.com/https://github.com/ZQCCBOY/fanli_vip.git;
     git clone https://ghproxy.com/https://github.com/ufuckee/jd_cookie.git;
     git clone https://ghproxy.com/https://github.com/cdle/jd_price.git;
     #编译
