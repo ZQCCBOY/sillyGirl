97163883c2094c3ae3096b8e88dcfddf80e3e91d
ZQCCBOY <97740114+ZQCCBOY@users.noreply.github.com>
2022-01-15 13:43:50 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+//青龙2.9机器人    
 	_ "github.com/cdle/sillyGirl/develop/qinglong"
+	
+//腾讯QQ	
 	_ "github.com/cdle/sillyGirl/im/qq"
+	
+//TeLegram	
 	_ "github.com/cdle/sillyGirl/im/tg"
+	
+//微信公众号	
 	_ "github.com/cdle/sillyGirl/im/wxmp"
+	
+//微信	
 	_ "github.com/cdle/sillyGirl/im/wx"
+	
+//猫咪比价	
 	//_ "github.com/cdle/sillyGirl/develop/jd_price"
+	
+//芝士	
 	_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+	
+	
   	//_ "github.com/cdle/sillyGirl/develop/cable"
+  	
+  	
+//京东返利  	
   	_ "github.com/cdle/sillyGirl/develop/fanli_jd"
+  	
+  	
+//淘宝返利  	
   	_ "github.com/cdle/sillyGirl/develop/fanli_tb"
+  	
+//一对一推送  	
 	//_ "github.com/cdle/sillyGirl/develop/onebyone"
+	
+//拼多多返利	
 	_ "github.com/cdle/sillyGirl/develop/fanli_pdd"
+
+//唯品会返利	
  	_ "github.com/cdle/sillyGirl/develop/fanli_vip"
+ 	
+//京东羊毛	
+ 	//_ "github.com/cdle/sillyGirl/develop/yangmao"
+ 	
 )
